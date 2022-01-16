8d1328205817a4712aa8978d2e50cd5fe27d5e6e
Thuku7 <robethuku11@gmail.com>
2022-01-17 02:59:03 +0300

    Create a pizza class and constructor

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+
 class Pizza {
- constructor(type,size,toppings,crust) {
+
+
+  constructor(type,size,toppings,crust) {
     this.type = type,
     this.size = size,
     this.toppings = toppings,
     this.crust = crust
-}
+  }
 }
 
 
