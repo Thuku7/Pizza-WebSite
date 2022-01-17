de224996a7d1a723c72f7176930cec59c4d4b3b2
Thuku7 <robethuku11@gmail.com>
2022-01-17 03:14:58 +0300

    Add event listener function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ class Pizza {
 }
 
 
+$(document).ready(function(){
+
+
+  $("#form").submit(function(event){
+    event.preventDefault()
+
+
+    let pizzaType = $("select#type").val()
+    let pizzaSize = $("select#size").val()
+    let pizzaToppings = $("select#toppings").val()
+    let pizzaCrust = $("select#crust")
+    let numberzPizzas = $("input#numbers").val()
+    let inHouse = $("input#inHouse")
+
+
+    //prices
+
+    let Prices = {
+      large:1000,
+      medium:800,
+      small:500,
+    }
+
+    
+  })
+})
